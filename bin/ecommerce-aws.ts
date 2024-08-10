04a86ec17d27546cadd31c7305fc9a7515395e8c
Lucas <cavalcantelucasx@gmail.com>
2024-08-10 13:53:06 -0300

    feat: adjust lambda to use lambda layers

--- bin/ecommerce-aws.ts
+++ bin/ecommerce-aws.ts
@@ -3,6 +3,7 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { ProductsAppStack } from "../lib/productsApp-stack";
 import { EcommerceApiStack } from "../lib/ecommerceApi-stack";
+import { ProductsAppLayersStack } from "../lib/productsAppLayers-stack";
 
 const app = new cdk.App();
 
@@ -15,10 +16,20 @@ const tags = {
   cost: "Ecommerce",
 };
 
+const productsAppLayersStack = new ProductsAppLayersStack(
+  app,
+  "ProductsAppLayers",
+  {
+    tags,
+    env,
+  }
+);
+
 const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
   tags,
   env,
 });
+productsAppStack.addDependency(productsAppLayersStack);
 
 const ecommerceApiStack = new EcommerceApiStack(app, "EcommerceApi", {
   productsFetchHandler: productsAppStack.productsFetchHandler,
