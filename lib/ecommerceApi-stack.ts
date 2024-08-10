b6d9b1e3deee07f342828fc91c8097a410c27c1f
Lucas <cavalcantelucasx@gmail.com>
2024-08-10 13:20:36 -0300

    feat: add ecommerce api and products app stacks

---
+++ lib/ecommerceApi-stack.ts
@@ -0,0 +1,43 @@
+import * as cdk from "aws-cdk-lib";
+import * as cwlogs from "aws-cdk-lib/aws-logs";
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+
+interface EcommerceApiStackProps extends cdk.StackProps {
+  readonly productsFetchHandler: lambdaNodeJS.NodejsFunction;
+}
+
+export class EcommerceApiStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: EcommerceApiStackProps) {
+    super(scope, id, props);
+
+    const logGroup = new cwlogs.LogGroup(this, "EcommerceApiLogs");
+    const api = new apigateway.RestApi(this, "EcommerceApi", {
+      restApiName: "EcommerceApi",
+      cloudWatchRole: true,
+      deployOptions: {
+        accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+          httpMethod: true,
+          ip: true,
+          protocol: true,
+          requestTime: true,
+          resourcePath: true,
+          responseLength: true,
+          status: true,
+          caller: true,
+          user: true,
+        }),
+      },
+    });
+
+    const productsFetchIntegration = new apigateway.LambdaIntegration(
+      props.productsFetchHandler
+    );
+
+    // "/products"
+    const productsResource = api.root.addResource("products");
+    productsResource.addMethod("GET", productsFetchIntegration);
+  }
+}
---
+++ lib/productsApp-stack.ts
@@ -0,0 +1,48 @@
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamoDB from "aws-cdk-lib/aws-dynamodb";
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+
+export class ProductsAppStack extends cdk.Stack {
+  readonly productsFetchHandler: lambdaNodeJS.NodejsFunction;
+  readonly productsDdb: dynamoDB.Table;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.productsDdb = new dynamoDB.Table(this, "ProductsDdb", {
+      tableName: "products",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: "id",
+        type: dynamoDB.AttributeType.STRING,
+      },
+      billingMode: dynamoDB.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+    });
+
+    this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(
+      this,
+      "ProductsFetchFunction",
+      {
+        runtime: lambda.Runtime.NODEJS_20_X,
+        functionName: "ProductsFetchFunction",
+        entry: "lambda/products/productsFetchFunction.ts",
+        handler: "handler",
+        memorySize: 512,
+        timeout: cdk.Duration.seconds(5),
+        bundling: {
+          minify: true,
+          sourceMap: false,
+        },
+        environment: {
+          PRODUCTS_DDB: this.productsDdb.tableName,
+        },
+      }
+    );
+
+    this.productsDdb.grantReadData(this.productsFetchHandler);
+  }
+}
